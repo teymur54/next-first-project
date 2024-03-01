81ec373d8461c4ffb676b20c2d72034337ae6ac2
Teymur <teymurhasanov100@gmail.com>
2024-03-01 17:24:44 +0400

    message

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -5,7 +5,11 @@ const AboutPage = () => {
   return (
     <div>
       <div className={styles.imgContainer}>
-        <Image src="/about.png" alt="" fill />
+        <Image
+          src="https://images.pexels.com/photos/18022560/pexels-photo-18022560/free-photo-of-close-up-of-a-raccoon.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+          fill
+        />
       </div>
     </div>
   );
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,28 @@
+import Image from "next/image";
+import styles from "./home.module.css";
+
 const Home = () => {
-  return <div>Homepage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Thoughts Agency</h1>
+        <p className={styles.desc}>
+          Enim ea sunt anim exercitation nostrud enim sint ea aute excepteur
+          incididunt tempor aute id.
+        </p>
+        <div className={styles.buttons}>
+          <button>Learn more</button>
+          <button>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src="/brands.png" alt="" fill className={styles.brandImg} />
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImg} />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
